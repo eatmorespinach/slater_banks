47ab4ad0c79ca8a29b2e49ad865959dedc6a2606
Drew Sing <sing.drew@gmail.com>
2013-07-03 21:15:41 -0400

    at 7min of autehntication from scratch railscast exercise

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Slaterbanks::Application.config.secret_token = 'd0fc1a497426b9edbb239122277164e6e2773ca643624ab1187398d50e35dd65fae47546ac252e3371ac4bcb20f6847a0599c03713f221324b85c4c3daf4d339'
